5daf35ccf8172bf191d2b2cc5e8cc19290dcfa85
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2022-02-09 22:34:08 +0100

    init commit

---
+++ app/src/androidTest/java/fr/Akkuun/com/example/plantemanaging/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package fr.Akkuun.com.example.plantemanaging
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("fr.Akkuun.com.example.plantemanaging", appContext.packageName)
+    }
+}
