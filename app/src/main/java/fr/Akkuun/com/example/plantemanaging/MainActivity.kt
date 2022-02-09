47aadcbb06d3ac5c158020724318ec2ea8e2e6aa
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2022-02-09 23:16:23 +0100

    premier build passe

--- app/src/main/java/fr/Akkuun/com/example/plantemanaging/MainActivity.kt
+++ app/src/main/java/fr/Akkuun/com/example/plantemanaging/MainActivity.kt
@@ -2,10 +2,18 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.Akkuun.com.example.plantemanaging.fragments.HomeFragments
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injecter le fragment dans notre boite
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,HomeFragments())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
+
 }
---
+++ app/src/main/java/fr/Akkuun/com/example/plantemanaging/fragments/HomeFragments.kt
@@ -0,0 +1,18 @@
+package fr.Akkuun.com.example.plantemanaging.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fr.Akkuun.com.example.plantemanaging.R
+
+class HomeFragments :Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater?.inflate(R.layout.fragment_home,container,false)
+    }
+}
