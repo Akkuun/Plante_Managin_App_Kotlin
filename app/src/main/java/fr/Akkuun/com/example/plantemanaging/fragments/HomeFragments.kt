1db428bd5c2358b79c04b25f519ffe895723b70a
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2022-02-09 23:32:52 +0100

    image arrondi qui defilent

--- app/src/main/java/fr/Akkuun/com/example/plantemanaging/fragments/HomeFragments.kt
+++ app/src/main/java/fr/Akkuun/com/example/plantemanaging/fragments/HomeFragments.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import fr.Akkuun.com.example.plantemanaging.R
+import fr.Akkuun.com.example.plantemanaging.manager.PlantAdapter
 
 class HomeFragments :Fragment() {
     override fun onCreateView(
@@ -13,6 +15,10 @@ class HomeFragments :Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater?.inflate(R.layout.fragment_home,container,false)
+        val view= inflater?.inflate(R.layout.fragment_home,container,false)
+    //recuper le recycle view
+val horizontalRecycleView=view.findViewById<RecyclerView>(R.id.horizontal_recycle_View)
+        horizontalRecycleView.adapter=PlantAdapter()
+        return view
     }
 }
---
+++ app/src/main/java/fr/Akkuun/com/example/plantemanaging/manager/PlantAdapter.kt
@@ -0,0 +1,33 @@
+package fr.Akkuun.com.example.plantemanaging.manager
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import fr.Akkuun.com.example.plantemanaging.R
+
+class PlantAdapter :RecyclerView.Adapter<PlantAdapter.ViewHolder>(){
+    //boite pour ranger tout les composants à controler
+
+    class ViewHolder(view:View):RecyclerView.ViewHolder(view){
+//image de la plante
+val planteImage=view.findViewById<ImageView>(R.id.image_item)
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+val view = LayoutInflater.from(parent.context).inflate(R.layout.item_horizontal_plant,parent,false)
+
+    return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return  5
+    }
+}
